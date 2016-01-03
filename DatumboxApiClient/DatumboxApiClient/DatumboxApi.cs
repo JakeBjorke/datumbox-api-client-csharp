fcec6ee2d55df684e44a60d0b440b901bf23ffad
Jake Bjorke <jrbjorke@gmail.com>
2016-01-03 14:47:56 -0600

    added initial files.

---
+++ DatumboxApiClient/DatumboxApiClient/DatumboxApi.cs
@@ -0,0 +1,18 @@
+﻿namespace DatumboxApiClient
+{
+    public class DatumboxApi
+    {
+        private const string SENTIMENT_ANALYSIS_URL = "http://api.datumbox.com/1.0/SentimentAnalysis.json";
+
+        private const string TWITTER_SENTIMENT_ANALYSIS_URL = "http://api.datumbox.com/1.0/TwitterSentimentAnalysis.json";
+
+
+
+        private readonly string _apiKey;
+
+        public DatumboxApi(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+    }
+}
