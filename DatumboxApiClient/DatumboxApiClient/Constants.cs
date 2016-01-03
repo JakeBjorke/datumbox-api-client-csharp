73039623afb1d7db5853212c8a40778cb7cc7383
Jake Bjorke <jrbjorke@gmail.com>
2016-01-03 15:52:16 -0600

    Moved constants to their own class.

---
+++ DatumboxApiClient/DatumboxApiClient/Constants.cs
@@ -0,0 +1,33 @@
+﻿namespace DatumboxApiClient
+{
+    internal static class Constants
+    {
+        public const string API_BASE_URL = "http://api.datumbox.com/1.0/";
+
+        public const string SENTIMENT_ANALYSIS_PATH = "SentimentAnalysis.json";
+
+        public const string TWITTER_SENTIMENT_ANALYSIS_PATH = "TwitterSentimentAnalysis.json";
+
+        public const string TOPIC_CLASSIFICATION_PATH = "TopicClassification.json";
+
+        public const string SPAM_DETECTION_PATH = "SpamDetection.json";
+
+        public const string ADULT_CONTENT_DETECTION_PATH = "AdultContentDetection.json";
+
+        public const string READABILITY_ASSESSMENT_PATH = "ReadabilityAssessment.json";
+
+        public const string LANGUAGE_DETECTION_PATH = "LanguageDetection.json";
+
+        public const string COMERCIAL_DETECTION_PATH = "CommercialDetection.json";
+
+        public const string EDUCATIONAL_DETECTION_PATH = "EducationalDetection.json";
+
+        public const string GENDER_DETECTION_PATH = "GenderDetection.json";
+
+        public const string TEXT_EXTRACTION_PATH = "TextExtraction.json";
+
+        public const string KEYWORD_EXTRACTION_PATH = "KeywordExtraction.json";
+
+        public const string DOCUMENT_SIMILARITY_PATH = "DocumentSimilarity.json";
+    }
+}
