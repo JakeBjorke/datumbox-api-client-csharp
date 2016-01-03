dffb6e56f86cfbbcb5cb8d8cc1f060f860a423f0
Jake Bjorke <jrbjorke@gmail.com>
2016-01-03 16:02:59 -0600

    changed constants naming convention

--- DatumboxApiClient/DatumboxApiClient/Constants.cs
+++ DatumboxApiClient/DatumboxApiClient/Constants.cs
@@ -2,32 +2,32 @@
 {
     internal static class Constants
     {
-        public const string API_BASE_URL = "http://api.datumbox.com/1.0/";
+        public const string ApiBaseUrl = "http://api.datumbox.com/1.0/";
 
-        public const string SENTIMENT_ANALYSIS_PATH = "SentimentAnalysis.json";
+        public const string SentimentAnalysisPath = "SentimentAnalysis.json";
 
-        public const string TWITTER_SENTIMENT_ANALYSIS_PATH = "TwitterSentimentAnalysis.json";
+        public const string TwitterSentimentAnalysisPath = "TwitterSentimentAnalysis.json";
 
-        public const string TOPIC_CLASSIFICATION_PATH = "TopicClassification.json";
+        public const string TopicClassificationPath = "TopicClassification.json";
 
-        public const string SPAM_DETECTION_PATH = "SpamDetection.json";
+        public const string SpamDetectionPath = "SpamDetection.json";
 
-        public const string ADULT_CONTENT_DETECTION_PATH = "AdultContentDetection.json";
+        public const string AdultContentDetectionPath = "AdultContentDetection.json";
 
-        public const string READABILITY_ASSESSMENT_PATH = "ReadabilityAssessment.json";
+        public const string ReadabilityAssessmentPath = "ReadabilityAssessment.json";
 
-        public const string LANGUAGE_DETECTION_PATH = "LanguageDetection.json";
+        public const string LanguageDetectionPath = "LanguageDetection.json";
 
-        public const string COMERCIAL_DETECTION_PATH = "CommercialDetection.json";
+        public const string ComercialDetectionPath = "CommercialDetection.json";
 
-        public const string EDUCATIONAL_DETECTION_PATH = "EducationalDetection.json";
+        public const string EducationalDetectionPath = "EducationalDetection.json";
 
-        public const string GENDER_DETECTION_PATH = "GenderDetection.json";
+        public const string GenderDetectionPath = "GenderDetection.json";
 
-        public const string TEXT_EXTRACTION_PATH = "TextExtraction.json";
+        public const string TextExtractionPath = "TextExtraction.json";
 
-        public const string KEYWORD_EXTRACTION_PATH = "KeywordExtraction.json";
+        public const string KeywordExtractionPath = "KeywordExtraction.json";
 
-        public const string DOCUMENT_SIMILARITY_PATH = "DocumentSimilarity.json";
+        public const string DocumentSimilarityPath = "DocumentSimilarity.json";
     }
 }
