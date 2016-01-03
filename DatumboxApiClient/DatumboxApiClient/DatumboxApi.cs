90a6e7fa94f8fbfae5154f75c580bffc0f141626
Jake Bjorke <jrbjorke@gmail.com>
2016-01-03 15:57:57 -0600

    added public interfaces

--- DatumboxApiClient/DatumboxApiClient/DatumboxApi.cs
+++ DatumboxApiClient/DatumboxApiClient/DatumboxApi.cs
@@ -10,5 +10,70 @@ namespace DatumboxApiClient
         {
             _apiKey = apiKey;
         }
+
+        public string SentimentAnalysis(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TwitterSentimentAnalysis(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SubjectivityAnalysis(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TopicClassification(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SpamDetection(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AdultContentDetection(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadabilityAssessment(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LanguageDetection(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EducationalDetection(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenderDetection(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TextExtraction(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string KeywordExtraction(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DocumentSimilarity(string text)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
